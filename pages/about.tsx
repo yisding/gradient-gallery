1bf6f107175b6b06a16bea883cdbbfd43c445a86
Yi Ding <yi.s.ding@gmail.com>
2023-03-16 21:35:27 -0700

    bug in nav

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,7 +12,7 @@ export default function About() {
         <h1 className="my-8 text-center text-4xl font-bold">
           Gradient Gallery
         </h1>
-        <Nav current="Home" />
+        <Nav current="About" />
         <p className="text-center my-32">
           This is a photo gallery of AI images maintained by{" "}
           <a href="https://thegradient.pub/">The Gradient</a>. If you have
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -12,7 +12,7 @@ export default function About() {
         <h1 className="my-8 text-center text-4xl font-bold">
           Gradient Gallery
         </h1>
-        <Nav current="Home" />
+        <Nav current="Contact" />
         <p className="my-32 text-center">
           Do you have some art you wish to display? Contact us using this{" "}
           <a href="https://docs.google.com/forms/d/e/1FAIpQLSe-1bW_bxr0-oS83pgy_ZF6IwG_UYFYd8KyJ-Cq4leVTL7i8A/viewform">
